355366e22749ac5cf226e63b3c66c09dd684db75
Marian Roibu <119500138+MarianRoibu@users.noreply.github.com>
2023-05-21 16:58:08 +0200

    Update UsersPhotos.jsx

--- src/UI/pages/users/Home/HomeComponents/UsersPhotos.jsx
+++ src/UI/pages/users/Home/HomeComponents/UsersPhotos.jsx
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom';
 const CirclePhotosContainer = styled.div`
 overflow: hidden;
 
-    overflow-x: scroll;
+    overflow-x: auto;
     position: relative;
     display: flex;
     flex-flow: row nowrap;
